3feada6b5ce17037bd7efa50c6a16d2f46f43f16
Albert Pinnerup <albert.pinnerup@gmail.com>
2025-02-06 12:22:42 +0100

    done

---
+++ app.js
@@ -0,0 +1,14 @@
+fetch("https://icanhazdadjoke.com", {
+    headers: {
+        "Accept": "application/json"
+    }
+})
+    .then(response => response.json())
+    .then(data => {
+        let root = document.querySelector("#root")
+
+        root.innerHTML = `
+            <h1>${data.joke}</h1>
+        `
+    })
+    
